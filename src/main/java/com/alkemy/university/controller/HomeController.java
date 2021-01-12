ed6d2899e699afeb70e4c3cc0ac6da7eb425715e
maira2691 <maira.pquiroz@gmail.com>
2021-01-12 16:56:21 +0100

    service find a course by Id

--- src/main/java/com/alkemy/university/controller/HomeController.java
+++ src/main/java/com/alkemy/university/controller/HomeController.java
@@ -3,11 +3,14 @@ package com.alkemy.university.controller;
 import com.alkemy.university.model.Course;
 import com.alkemy.university.service.CourseService;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,6 +44,19 @@ public class HomeController {
     @ApiResponse(code = 200, message = "OK")
     public ResponseEntity<List<Course>> getAll() {
         return new ResponseEntity<>(courseService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/todasMaterias/{id}")
+    @ApiOperation("Search a course with an ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Course not found")
+    })
+    public ResponseEntity<Course> getProduct(@ApiParam(value = "The id of the course", required = true, example = "7")
+                                              @PathVariable("id") Long idCourse) {
+        return courseService.getCourse(idCourse)
+                .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
 }
