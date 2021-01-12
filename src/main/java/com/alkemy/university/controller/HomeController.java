ca64e015a42eb9d9a307746c372c9daa7c44f1bf
maira2691 <maira.pquiroz@gmail.com>
2021-01-12 16:33:10 +0100

    service show all courses

--- src/main/java/com/alkemy/university/controller/HomeController.java
+++ src/main/java/com/alkemy/university/controller/HomeController.java
@@ -1,10 +1,24 @@
 package com.alkemy.university.controller;
 
+import com.alkemy.university.model.Course;
+import com.alkemy.university.service.CourseService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/")
 public class HomeController {
+
+    @Autowired
+    private CourseService courseService;
 
     @GetMapping("/")
     public String home() {
@@ -19,6 +33,14 @@ public class HomeController {
     @GetMapping("/admin")
     public String admin() {
         return "<h1>welcome admin</h1>";
+    }
+
+    //Servicio para ver todas las materias
+    @GetMapping("/materias")
+    @ApiOperation("Get all courses")
+    @ApiResponse(code = 200, message = "OK")
+    public ResponseEntity<List<Course>> getAll() {
+        return new ResponseEntity<>(courseService.getAll(), HttpStatus.OK);
     }
 
 }
